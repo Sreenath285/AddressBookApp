347e5bd3b3748dbf1d5c7abe14f674b4d75ef3da
Sreenath285 <sreenath.285@gmail.com>
2022-04-19 14:03:39 +0530

    View person address book details in tabular format using template literals

--- AddressBookApp/js/address_book_home.js
+++ AddressBookApp/js/address_book_home.js
@@ -5,22 +5,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
 const createInnerHtml = () => {
   const headerHtml = "<th>Full Name</th><th>Address</th><th>City</th><th>State</th>" + 
                      "<th>Zip Code</th><th>Phone Number</th><th>Actions</th>"
-  let addressBookData = createAddressBookJSON()[1];
-  const innerHtml = 
-   `${headerHtml}
-    <tr>
-        <td>${addressBookData._name}</td>
-        <td>${addressBookData._address}</td>
-        <td>${addressBookData._city}</td>
-        <td>${addressBookData._state}</td>
-        <td>${addressBookData._zipCode}</td>
-        <td>${addressBookData._phoneNumber}</td>
-        <td>
-            <img name="${addressBookData._id}" src="..\assets\icons\delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
-            <img name="${addressBookData._id}" src="..\assets\icons\create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
-        </td>
-   </tr>`;
-   document.querySelector('#table-display').innerHTML = innerHtml;
+  let innerHtml = `${headerHtml}`;  
+  let addressBookList = createAddressBookJSON();
+  for (const addressBookData of addressBookList) {
+    innerHtml = 
+    `${innerHtml}
+     <tr>
+         <td>${addressBookData._name}</td>
+         <td>${addressBookData._address}</td>
+         <td>${addressBookData._city}</td>
+         <td>${addressBookData._state}</td>
+         <td>${addressBookData._zipCode}</td>
+         <td>${addressBookData._phoneNumber}</td>
+         <td>
+             <img name="${addressBookData._id}" src="..\assets\icons\delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
+             <img name="${addressBookData._id}" src="..\assets\icons\create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
+         </td>
+    </tr>`;
+  }
+    document.querySelector('#table-display').innerHTML = innerHtml;
 }
 
 const createAddressBookJSON = () => {
@@ -34,12 +37,12 @@ const createAddressBookJSON = () => {
             _phoneNumber: '91 7075743215'
         },
         {
-            _name: 'Rahul',
-            _address: 'Adoni',
-            _city: 'Kurnool',
-            _state: 'Andhra Pradesh',
-            _zipCode: '518301',
-            _phoneNumber: '91 7075743215'
+            _name: 'Yashwanth',
+            _address: 'Marathahalli',
+            _city: 'Banglore',
+            _state: 'Karnataka',
+            _zipCode: '560063',
+            _phoneNumber: '91 7075743123'
         }
     ];
     return addressBookListLocal;
